eeb7907525bc8980192d712cb7603d0ee729de6f
thebao <1344895720@qq.com>
2025-03-22 17:04:04 +0800

    fix(parser): 修正函数名

--- include/protocol/parser.hh
+++ include/protocol/parser.hh
@@ -18,12 +18,12 @@ enum class HTTPMethod {
 // 解析 HTTP 请求行和头部字段
 class Parser {
  public:
-  // Method, Path, Version
+  // Method URI Version 不允许多空格
   static auto parseRequest(std::string& request)
       -> std::tuple<std::string, std::string, std::string>;
 
-  // Field, Value
-  static auto parserHeader(std::string& header) -> std::tuple<std::string, std::string>;
+  // Field: Value 只有value前允许多空格, 一般为可选空格(0-1)
+  static auto parseHeader(std::string& header) -> std::tuple<std::string, std::string>;
 };
 
 #endif // PROTOCOL_PARSER_H
